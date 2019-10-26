0b5e4cecb467fd5c9863b3780f07ae8d6c7c3995
Austin Roach <ahroach@gmail.com>
2019-10-26 06:28:21 -0400

    Updates to ubuntu-19.10

--- ubuntu-19.10/provision.sh
+++ ubuntu-19.10/provision.sh
@@ -1,89 +1,164 @@
 #!/bin/bash
 
-USER=`(whoami)`
+INSTALL_BASICS=true
+INSTALL_SWAY=true
+INSTALL_WIRESHARK=false
+INSTALL_BINARY_ANALYSIS=false
+INSTALL_LIBVIRT=false
+INSTALL_VIRTUALBOX=false
+INSTALL_DOCKER=false
+INSTALL_DOCKER_SCANNING_IMAGES=false
+INSTALL_FIRMWARE=false
+INSTALL_HARDWARE=false
+INSTALL_HARDWARE_CAN=false
+INSTALL_HARDWARE_AVR=false
+INSTALL_HARDWARE_ESP=false
 
-## The basics
-sudo apt-get install vim-gtk3 git unzip p7zip-full wget xxd screen mosh brightnessctl links curl dos2unix units
-sudo usermod -aG dialout $USER
-sudo usermod -aG video $USER
-# Install hexdiff
-git clone https://github.com/ahroach/hexdiff
-pushd hexdiff
-gcc -o hexdiff hexdiff.c
-sudo cp hexdiff /usr/local/bin
-popd
-rm -rf hexdiff
-# Install binexplore
-git clone https://github.com/ahroach/binexplore
-pushd binexplore
-sudo mkdir -p /usr/lib/python3/dist-packages/binexplore/
-sudo cp * /usr/lib/python3/dist-packages/binexplore/
-popd
-rm -rf binexplore
-# Install diffcount
-git clone https://github.com/ahroach/diffcount
-pushd diffcount
-gcc -mpopcnt -O3 -o diffcount diffcount.c
-sudo cp diffcount /usr/local/bin
-popd
-rm -rf diffcount
-# Install carve
-sudo cp ../bin/carve /usr/local/bin
+export DEBIAN_FRONTEND=noninteractive
+USER=`(logname)`
 
-## Sway window manager
-sudo apt-get install sakura sway swaylock swayidle i3status zenity jq suckless-tools
-mkdir -p ~/.config/sway
-cp ../config/sway.config ~/.config/sway/config
-mkdir -p ~/.config/sakura
-cp ../config/sakura.conf ~/.config/sakura/sakura.conf
+if [ $(id -u) -ne 0 ]
+then
+	echo "This script must be run as root. (Use sudo.)"
+	exit 1
+fi
 
-## Networking
-sudo apt-get install net-tools nmap netcat-openbsd netcat-traditional
-# sudo apt-get install wireshark
-# sudo groupadd wireshark
-# sudo usermod -aG wireshark $USER
+apt-get update
+if [ $? -ne 0 ]
+then
+	echo "Apt update failed. Check network connection or for competing apt processes."
+	exit 1
+fi
 
-## Python
-sudo apt-get install ipython3
-# sudo apt-get install python3-numpy python3-scipy python3-matplotlib
+if [ "$INSTALL_BASICS" = true ]
+then
+	apt-get -yq install vim-gtk3 git unzip p7zip-full xxd screen mosh brightnessctl dos2unix units bless
+	apt-get -yq install wget links curl net-tools nmap netcat-openbsd netcat-traditional
+	apt-get -yq install ipython3 python3-crypto python3-numpy python3-matplotlib
+	usermod -aG dialout $USER
+	usermod -aG video $USER
+	# Install hexdiff
+	git clone https://github.com/ahroach/hexdiff
+	pushd hexdiff
+	gcc -o hexdiff hexdiff.c
+	cp hexdiff /usr/local/bin
+	popd
+	rm -rf hexdiff
+	# Install binexplore
+	git clone https://github.com/ahroach/binexplore
+	pushd binexplore
+	mkdir -p /usr/lib/python3/dist-packages/binexplore/
+	cp * /usr/lib/python3/dist-packages/binexplore/
+	popd
+	rm -rf binexplore
+	# Install diffcount
+	git clone https://github.com/ahroach/diffcount
+	pushd diffcount
+	gcc -mpopcnt -O3 -o diffcount diffcount.c
+	cp diffcount /usr/local/bin
+	popd
+	rm -rf diffcount
+	# Install carve
+	cp ../bin/carve /usr/local/bin
+fi
 
-## libvirt virtualization
-# sudo apt-get install virt-manager
-# sudo usermod -aG libvirt $USER
-# sudo usermod -aG libvirt-qemu $USER
+if [ "$INSTALL_SWAY" = true ]
+then
+	apt-get -yq install sakura sway swaylock swayidle i3status zenity jq suckless-tools
+	mkdir -p /home/$USER/.config/sway
+	cp ../config/sway.config /home/$USER/.config/sway/config
+	chown $USER:$USER /home/$USER/.config/sway/config
+	mkdir -p /home/$USER/.config/sakura
+	cp ../config/sakura.conf /home/$USER/.config/sakura/sakura.conf
+	chown $USER:$USER /home/$USER/.config/sakura/sakura.conf
+fi
 
-## VirtualBox virtualization
-# sudo apt-get install virtualbox
+if [ "$INSTALL_WIRESHARK" = true ]
+then
+	# Install setuid to allow capture by wireshark group members
+	echo wireshark-common wireshark-common/install-setuid boolean true | debconf-set-selections
+	apt-get -yq install wireshark
+	groupadd wireshark
+	usermod -aG wireshark $USER
+fi
 
-## docker virtualization
-# sudo apt-get install docker.io
-# sudo groupadd docker
-# sudo usermod -aG docker $USER
+if [ "$INSTALL_BINARY_ANALYSIS" = true ]
+then
+	apt-get -yq install gdb gdbserver edb-debugger elfutils
+	git clone --depth 1 https://github.com/radareorg/radare2.git
+	# Install radare2
+	pushd radare2/sys
+	./install.sh --install
+	popd
+	rm -rf radare2
+	# Install Ghidra
+	apt-get -yq install openjdk-11-jdk openjdk-11-jdk-headless openjdk-11-jre openjdk-11-jre-headless
+	# Ugly hack to get current Ghidra download link
+	wget -r -A "*.zip" -l 1 -O ghidra-latest.zip https://www.ghidra-sre.org
+	unzip ghidra-latest.zip -d /usr/local
+	# If there happen to be multiple ghidra installations (there shouldn't), just
+	# link to the first one we find
+	ghidras=(/usr/local/ghidra*)
+	ln -s ${ghidras[0]}/ghidraRun /usr/local/bin/ghidra
+	rm ghidra-latest.zip
+	# Make things better on Wayland
+	echo "_JAVA_AWT_WM_NONREPARENTING=1" >> /etc/environment
+fi
 
-## Generic hardware
-# sudo apt-get install sigrok pulseview putty gtkterm python3-serial
-# sudo apt-get install openocd urjtag flashrom
+if [ "$INSTALL_LIBVIRT" = true ]
+then
+	apt-get -yq install libvirt0 libvirt-daemon libvirt-daemon-system virt-manager virt-viewer
+	groupadd libvirt
+	usermod -aG libvirt $USER
+	groupadd libvirt-qemu
+	usermod -aG libvirt-qemu $USER
+fi
 
-## CAN
-# sudo apt-get install python3-can can-utils
+if [ "$INSTALL_VIRTUALBOX" = true ]
+then
+	apt-get -yq install virtualbox
+	groupadd vboxusers
+	usermod -aG vboxusers $USER
+	groupadd vboxsf
+	usermod -aG vboxsf $USER
+fi
 
-## AVR
-# sudo apt-get install avr-libc gcc-avr avrdude gdb-avr
+if [ "$INSTALL_DOCKER" = true ]
+then
+	apt-get -yq install docker.io
+	groupadd docker
+	usermod -aG docker $USER
+fi
 
-## Binary analysis
-# sudo apt-get install gdb gdbserver edb-debugger elfutils 
-# git clone --depth 1 https://github.com/radareorg/radare2.git
-# pushd radare2/sys
-# sudo ./install.sh
-# popd
-# sudo rm -rf radare2
-# sudo apt-get install openjdk-11-jdk openjdk-11-jdk-headless openjdk-11-jre openjdk-11-jre-headless
-# wget -r -A "*.zip" -l 1 -O ghidra-latest.zip https://www.ghidra-sre.org
-# sudo unzip ghidra-latest.zip -d /usr/local
-# sudo ln -s /usr/local/ghidra/ghidraRun /usr/local/bin/ghidra
-# rm ghidra-latest.zip
+if [ "$INSTALL_DOCKER_SCANNING_IMAGES" = true ]
+then
+	docker pull metasploitframework/metasploit-framework
+	docker pull mikesplain/openvas
+fi
 
-## Vulnerability testing
-# sudo docker pull metasploitframework/metasploit-framework
-# sudo docker pull mikesplain/openvas-docker
+if [ "$INSTALL_FIRMWARE" = true ]
+then
+	apt-get -yq install binwalk squashfs-tools
+fi
+
+if [ "$INSTALL_HARDWARE" = true ]
+then
+	apt-get -yq install sigrok pulseview putty gtkterm python3-serial
+	apt-get -yq install openocd urjtag flashrom
+fi
+
+if [ "$INSTALL_HARDWARE_CAN" = true ]
+then
+	apt-get -yq install python3-can can-utils
+fi
+
+if [ "$INSTALL_HARDWARE_AVR" = true ]
+then
+	apt-get -yq install avr-libc gcc-avr avrdude gdb-avr simavr
+fi
+
+if [ "$INSTALL_HARDWARE_ESP" = true ]
+then
+	apt-get -yq install esptool
+fi
 
