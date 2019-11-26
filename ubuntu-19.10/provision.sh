9042451a9c524257317e57fc3f002f1f7ed74994
Austin Roach <ahroach@gmail.com>
2019-11-26 04:10:56 -0500

    build-essential isn't automatically installed...

--- ubuntu-19.10/provision.sh
+++ ubuntu-19.10/provision.sh
@@ -35,6 +35,7 @@ if [ "$INSTALL_BASICS" = true ]
 then
 	apt-get -yq install vim-gtk3 git unzip p7zip-full xxd screen mosh dos2unix units bless
 	apt-get -yq install wget links curl net-tools nmap netcat-openbsd netcat-traditional
+	apt-get -yq install build-essential
 	apt-get -yq install ipython3 python3-crypto python3-numpy python3-matplotlib
 	usermod -aG dialout $USER
 	usermod -aG video $USER
