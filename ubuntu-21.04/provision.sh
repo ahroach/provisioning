c9f3e82bead94ad917a782145a873130686a8f1e
Austin Roach <ahroach@gmail.com>
2022-01-09 10:08:32 -0500

    Add magnus screen magnifier

--- ubuntu-21.04/provision.sh
+++ ubuntu-21.04/provision.sh
@@ -121,7 +121,7 @@ fi
 if [ "$INSTALL_PRESENTATION_TOOLS" = true ]
 then
 	apt-get -yq install cheese guvcview v4l-utils
-	apt-get -yq install xournal gromit-mpx
+	apt-get -yq install xournal gromit-mpx magnus
 	apt-get -yq install texlive texlive-latex-recommended texlive-latex-extra
 	apt-get -yq install inkscape gimp krita
 fi
